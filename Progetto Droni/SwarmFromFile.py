d193d6c2bc02db1ae56001fd0eace07ab4f48b52
antonio_mignogna <120557495+antmig05@users.noreply.github.com>
2023-11-21 15:48:11 +0100

    Add files via upload

---
+++ Progetto Droni/SwarmFromFile.py
@@ -0,0 +1,87 @@
+from djitellopy import TelloSwarm
+import threading
+import time
+
+# Carica gli indirizzi IP da file
+swarm1 = TelloSwarm.fromFile("ips1.txt") #File di testo con gli indirizzi IP del primo swarm
+swarm2 = TelloSwarm.fromFile("ips2.txt")
+Instructions1 = ("Swarm1.txt") #File di testo con le istruzioni di volo del primo swarm
+Instructions2 = ("Swarm2.txt") 
+
+# Creare una barriera per sincronizzare i due thread
+barrier = threading.Barrier(2)
+
+def controllo_tello(tello, nome):
+    try:
+        tello.connect()
+        tello.query_battery()
+        print(f"[INFO] Connessione riuscita per {nome}")
+    except Exception as e:
+        print(f"[ERRORE] Connessione fallita per {nome}: {e}")
+        return False
+
+    return True
+
+def takeoff_and_fly(tello, nome):
+    try:
+        print(f"[INFO] {nome}: In attesa della sincronizzazione...")
+        # Aspetta che entrambi i droni siano pronti prima di continuare
+        barrier.wait()
+
+        print(f"[INFO] {nome}: Decollo in corso...")
+        with open(Instructions1, 'r') as file:
+            for line in file:
+                command = line.strip()
+                try:
+                    eval(f'{command}')
+                except Exception as e:
+                    print(f"[ERRORE] Errore durante l'esecuzione del comando '{command}' : {e}")
+        print(f"[INFO] {nome}: Volare completato.")
+    except Exception as e:
+        print(f"[ERRORE] {nome}: Errore durante il volo: {e}")
+def takeoff_and_fly2(tello, nome):
+    try:
+        print(f"[INFO] {nome}: In attesa della sincronizzazione...")
+        # Aspetta che entrambi i droni siano pronti prima di continuare
+        barrier.wait()
+
+        print(f"[INFO] {nome}: Decollo in corso...")
+        with open(Instructions2, 'r') as file:
+            for line in file:
+                command = line.strip()
+                try:
+                    eval(f'{command}')
+                except Exception as e:
+                    print(f"[ERRORE] Errore durante l'esecuzione del comando '{command}' : {e}")
+        print(f"[INFO] {nome}: Volare completato.")
+    except Exception as e:
+        print(f"[ERRORE] {nome}: Errore durante il volo: {e}")
+# Creare i thread di controllo
+thread_controllo_1 = threading.Thread(target=controllo_tello, args=(swarm1, "Swarm1"))
+thread_controllo_2 = threading.Thread(target=controllo_tello, args=(swarm2, "Swarm2"))
+
+# Avvia i thread di controllo
+thread_controllo_1.start()
+thread_controllo_2.start()
+
+# Attendi la fine dei thread di controllo
+thread_controllo_1.join()
+thread_controllo_2.join()
+
+# Se entrambi i droni sono pronti, procedi con il volo
+if thread_controllo_1.is_alive() or thread_controllo_2.is_alive():
+    print("[ERRORE] Almeno uno dei droni non è pronto. Interrompiamo.")
+else:
+    # Creare i thread di volo
+    thread_volo_1 = threading.Thread(target=takeoff_and_fly, args=(swarm1, "Swarm1"))
+    thread_volo_2 = threading.Thread(target=takeoff_and_fly2, args=(swarm2, "Swarm2"))
+
+    # Avvia i thread di volo
+    thread_volo_1.start()
+    thread_volo_2.start()
+
+    # Attendi la fine dei thread di volo
+    thread_volo_1.join()
+    thread_volo_2.join()
+
+print("Programma completato.")
